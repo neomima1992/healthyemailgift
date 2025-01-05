f5f448b67a33b8caafd277830c86877832ee8fdd
neomima1992 <danielsneomi@gmail.com>
2025-01-05 23:28:03 +0200

    add thanks route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import About from "./pages/ThankYou";
 
 const queryClient = new QueryClient();
 
@@ -15,6 +16,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+      <Route path="/thank-you" element={<ThankYou />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
